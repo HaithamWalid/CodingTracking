a5ec3b0c8fa7701c41b4d189bdaa37e6dbcdb20e
Haitham Walid <h.walid@conix.ai>
2023-12-20 12:33:54 +0200

    intail Commit

--- codingTracking/Program.cs
+++ codingTracking/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!,one more modification");
+﻿void MyFirstMethod()
+{
+    // Do something
+}
+
+
+void MySecondMethod()
+{
+    // Do something
+}
+
+void MyThirdMethod()
+{
+    // Do something
+}
+
 
